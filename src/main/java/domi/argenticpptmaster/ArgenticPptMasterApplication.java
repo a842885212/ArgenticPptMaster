6adf4b14a48e0bcd221f62da77aad8fb8d73f712
zhangtianhao <zhangtianhao@medlive.cn>
2026-07-02 14:30:59 +0800

    init

---
+++ src/main/java/domi/argenticpptmaster/ArgenticPptMasterApplication.java
@@ -0,0 +1,13 @@
+package domi.argenticpptmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgenticPptMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgenticPptMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/domi/argenticpptmaster/ArgenticPptMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package domi.argenticpptmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgenticPptMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
